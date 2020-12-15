5c63c531d48472a61681284e00f0032187521c1b
KolbyMcGarrah <kmcgarrah@kochava.com>
2020-12-15 11:37:00 -0700

    Added SetNX, Expire and Close to V4

--- v4/wrapper.go
+++ v4/wrapper.go
@@ -69,10 +69,41 @@ func (w *Wrapper) Ping(ctx context.Context) (cmd ocredis.StatusCmd) {
 
 // Del integrates the redis Del command with metrics
 func (w *Wrapper) Del(ctx context.Context, keys ...string) (cmd ocredis.IntCmd) {
-	var recordCallFunc = ocredis.RecordCall(ctx, "go.redis.incr", w.instanceName)
+	var recordCallFunc = ocredis.RecordCall(ctx, "go.redis.del", w.instanceName)
 	defer func() {
 		recordCallFunc(cmd)
 	}()
 	cmd = w.client.Del(keys...)
+	return
+}
+
+// SetNX integrates the redis SetNX command with metrics
+func (w *Wrapper) SetNX(ctx context.Context, key string, value interface{}, expiration time.Duration) (cmd ocredis.BoolCmd) {
+	var recordCallFunc = ocredis.RecordCall(ctx, "go.redis.setnx", w.instanceName)
+	defer func() {
+		recordCallFunc(cmd)
+	}()
+	cmd = w.client.SetNX(key, value, expiration)
+	return
+}
+
+// Close integrates the redis Close command with metrics
+func (w *Wrapper) Close(ctx context.Context) (err error) {
+	var recordCallFunc = ocredis.RecordCall(ctx, "go.redis.close", w.instanceName)
+	defer func() {
+		// Pass in a blank cmd because there is no command type returned from close
+		recordCallFunc(&pkgredis.Cmd{})
+	}()
+	err = w.client.Close()
+	return
+}
+
+// Expire integrates the redis Expire command with metrics
+func (w *Wrapper) Expire(ctx context.Context, key string, expiration time.Duration) (cmd ocredis.BoolCmd) {
+	var recordCallFunc = ocredis.RecordCall(ctx, "go.redis.expire", w.instanceName)
+	defer func() {
+		recordCallFunc(cmd)
+	}()
+	cmd = w.client.Expire(key, expiration)
 	return
 }
